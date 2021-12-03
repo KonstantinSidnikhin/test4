67068fff0d86dd87a9152c26b255a75f3acb848f
Konstantin Sidnikhin <sidnikhin@yandex.ru>
2021-12-03 14:10:23 +0400

    refactor

--- Cart.php
+++ Cart.php
@@ -10,8 +10,7 @@ class Cart{
         $this->products[]=$item;
         return $this->get();
     }
-    public function remove($item1,$get){
-        $this->item1=$item1;
+    public function remove($item1){
         $index=array_search($item1,$this->products);
         unset($this->products[$index]);
   return $this->get();
@@ -20,13 +19,11 @@ class Cart{
 $cart=new Cart;
 $cart->add("milk");
 $cart->add("butter");
+$cart->add("yogurt");
 $cart->remove("butter",$cart->get());
 print_r($cart->products);
 
-//$ar=["milk","butter"];
-//$index=array_search("butter",$ar);
-//unset($ar[$index]);
-//print_r($ar);
+
 
 
 
