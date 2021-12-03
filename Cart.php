5c5df6d5a3d639edb29747139b9e0ac167f6c1a3
Konstantin Sidnikhin <sidnikhin@yandex.ru>
2021-12-03 14:26:52 +0400

    remove refactor2

--- Cart.php
+++ Cart.php
@@ -4,23 +4,26 @@ class Cart{
     public $products = [];
 //    public  $index=0;
     public function get(){
-    return $this->products;
+    return $this;
 }
     public function add($item){
         $this->products[]=$item;
-        return $this->get();
+        return $this;
     }
     public function remove($item1){
         $index=array_search($item1,$this->products);
         unset($this->products[$index]);
-  return $this->get();
+  return $this;
     }
 }
 $cart=new Cart;
 $cart->add("milk");
 $cart->add("butter");
 $cart->add("yogurt");
-$cart->remove("butter",$cart->get());
+$cart->add("bacon")->add("ham");
+$cart->remove("ham",$cart->get());
+
+
 print_r($cart->products);
 
 
