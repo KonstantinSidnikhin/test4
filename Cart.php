271cc11b84f3e0fcb7327c2ca942d9d35f6b0949
Konstantin Sidnikhin <sidnikhin@yandex.ru>
2021-12-03 13:15:05 +0400

    remove function solve

--- Cart.php
+++ Cart.php
@@ -2,37 +2,31 @@
 
 class Cart{
     public $products = [];
-    public  $index=0;
+//    public  $index=0;
     public function get(){
     return $this->products;
 }
     public function add($item){
         $this->products[]=$item;
-        return $this->products;
-
+        return $this->get();
     }
-    public function remove($item1){
-
-
-
-        $index=array_search($item1, $products);
-
-
-        array_splice($products,$index,1);
-  return $this->$products;
+    public function remove($item1,$get){
+        $this->item1=$item1;
+        $index=array_search($item1,$this->products);
+        unset($this->products[$index]);
+  return $this->get();
     }
-
-
 }
-
-
-
 $cart=new Cart;
 $cart->add("milk");
 $cart->add("butter");
-$cart->remove("butter");
+$cart->remove("butter",$cart->get());
 print_r($cart->products);
 
+//$ar=["milk","butter"];
+//$index=array_search("butter",$ar);
+//unset($ar[$index]);
+//print_r($ar);
 
 
 
